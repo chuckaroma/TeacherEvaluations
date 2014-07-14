a49d8d669e6331e29ecc98832ad57dee729e2646
Charles Bailey <cbailey92@gmail.com>
2014-07-14 17:27:31 -0600

    Initial Commit

---
+++ TeacherEvaluations/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TeacherEvaluations
+//
+//  Created by Charles Bailey on 7/14/14.
+//  Copyright (c) 2014 charlesbailey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ TeacherEvaluationsTests/TeacherEvaluationsTests.m
@@ -0,0 +1,34 @@
+//
+//  TeacherEvaluationsTests.m
+//  TeacherEvaluationsTests
+//
+//  Created by Charles Bailey on 7/14/14.
+//  Copyright (c) 2014 charlesbailey. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TeacherEvaluationsTests : XCTestCase
+
+@end
+
+@implementation TeacherEvaluationsTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
